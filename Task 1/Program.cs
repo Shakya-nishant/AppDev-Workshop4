d07eb602e4016354e732bd2f31a7c5df50e569d8
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-11-18 09:17:18 +0545

    Task 1 compleated

---
+++ Task 1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student S1 = new Student();
+        Student S2 = new Student();
+
+        S1.Name = "Ram";
+        S1.Age = 21;
+        S1.Address = "Dharan, Nepal";
+
+        S2.Name = "Shyam";
+        S2.Age = 22;
+        S2.Address = "Kathmandu, Nepal";
+
+        Console.WriteLine("Student 1 Details:");
+        Console.WriteLine("Name: " + S1.Name);
+        Console.WriteLine("Age: " + S1.Age);
+        Console.WriteLine("Address: " + S1.Address);
+
+        Console.WriteLine("\nStudent 2 Details:");
+        Console.WriteLine("Name: " + S2.Name);
+        Console.WriteLine("Age: " + S2.Age);
+        Console.WriteLine("Address: " + S2.Address);
+
+        Console.WriteLine("\nCollege Name: " + Student.CollegeName);
+    }
+}
---
+++ Task 1/Student.cs
@@ -0,0 +1,12 @@
+
+
+namespace Task_1;
+
+public class Student
+{
+    public string Name;
+    public int Age;
+    public string Address;
+
+    public static string CollegeName = "IIC";
+}
